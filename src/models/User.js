de0bea79318ff40b6f466df780376d6ee83ecb88
Trần Huy Hoàng <hoanghuytran10102003@gmail.com>
2025-03-10 15:15:29 +0700

    OSQ-5: update model User based on requirements of application

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
+  profilePicture: { type: String },
 });
 export default mongoose.model("User", UserSchema);
